d7e37633a40ec938148665f7d876fea8b8fa408b
WSPHCY <851778080@QQ.COM>
2016-10-31 19:12:44 +0800

    update 03

--- src/com/atgugu/GitExcise.java
+++ src/com/atgugu/GitExcise.java
@@ -6,5 +6,9 @@ public class GitExcise {
 		
 		System.out.println("11111111111111111");
 		System.out.println("22222222222222222");
+		System.out.println("3333333333333333333");
+		System.out.println("3333333333333333333");
+		System.out.println("3333333333333333333");
+		System.out.println("3333333333333333333");
 	}
 }
