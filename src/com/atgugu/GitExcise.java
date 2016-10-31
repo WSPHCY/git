509fcd0edc1cc6799f8b500ad4c558e74bc1bbef
shaopeng <851778080@qq.com>
2016-10-31 19:30:22 +0800

    update 04

--- src/com/atgugu/GitExcise.java
+++ src/com/atgugu/GitExcise.java
@@ -6,12 +6,5 @@ public class GitExcise {
 		
 		System.out.println("11111111111111111");
 		System.out.println("22222222222222222");
-		System.out.println("22222222222222222");
-		System.out.println("22222222222222222");
-		System.out.println("22222222222222222");
-		System.out.println("22222222222222222");
-		System.out.println("22222222222222222");
-		System.out.println("22222222222222222");
-		System.out.println("22222222222222222");
 	}
 }
